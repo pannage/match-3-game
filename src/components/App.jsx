3a0405e6512486cdc4b06a31d2ff7370b40e4a33
Zrudikkk <zaburianov@gmail.com>
2021-01-10 17:20:03 +0200

    fix: change basic functions structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,78 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/App.css';
 
-const CreateBoard = React.memo((props) => {
+function CreateBoard(props) {
     const squaresArray = [];
-
-    let colorBeingDragged;
-    let colorBeingReplaced;
-    let squareIdBeingDragged;
-    let squareIdBeingReplaced;
-
-    function dragStart(e) {
-        colorBeingDragged = e.target.style.backgroundImage;
-        squareIdBeingDragged = parseInt(e.target.id, 10);
+    for (let i = 0; i < props.squares.length; i += 1) {
+        squaresArray.push(<div draggable="true" id={i} className="square" key={i} style={{ backgroundImage: props.squares[i] }} />);
     }
 
-    function dragOver(e) {
-        e.preventDefault();
-    }
-
-    function dragEnter(e) {
-        e.preventDefault();
-    }
-
-    function dragLeave(e) {
-        e.preventDefault();
-    }
-
-    function dragDrop(e) {
-        const squares = document.querySelectorAll('.square');
-        colorBeingReplaced = e.target.style.backgroundImage;
-        squareIdBeingReplaced = parseInt(e.target.id, 10);
-        e.target.style.backgroundImage = colorBeingDragged;
-        squares[squareIdBeingDragged].style.backgroundImage = colorBeingReplaced;
-    }
-
-    function dragEnd() {
-        const validMoves = [
-            squareIdBeingDragged - 1,
-            squareIdBeingDragged - props.width,
-            squareIdBeingDragged + 1,
-            squareIdBeingDragged + props.width,
-        ];
-        let validMove = validMoves.indexOf(squareIdBeingReplaced);
-        if (validMove === 0) {
-            if (squareIdBeingReplaced % props.width === 7) validMove = -1;
-        } else if (validMove === 2) {
-            if (squareIdBeingReplaced % props.width === 0) validMove = -1;
-        }
-        const squares = document.querySelectorAll('.square');
-        if (squareIdBeingReplaced !== undefined && validMove !== -1) {
-            squareIdBeingReplaced = null;
-        } else if (squareIdBeingReplaced !== undefined && validMove === -1) {
-            squares[squareIdBeingReplaced].style.backgroundImage = colorBeingReplaced;
-            squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
-        } else {
-            squares[squareIdBeingDragged].style.backgroundImage = colorBeingDragged;
-        }
-    }
-
-    for (let i = 0; i < props.width ** 2; i += 1) {
-        const randomColor = Math.floor(Math.random() * props.candyColors.length);
-        const square = <div draggable id={i} className="square" onDragStart={dragStart} onDragEnd={dragEnd} onDragOver={dragOver} onDragEnter={dragEnter} onDragLeave={dragLeave} onDrop={dragDrop} key={i} style={{ backgroundImage: props.candyColors[randomColor] }} />;
-        squaresArray.push(square);
-    }
-
-    return (<div className="grid">{squaresArray}</div>);
-});
+    return (squaresArray);
+}
 
 CreateBoard.propTypes = {
-    width: PropTypes.number.isRequired,
-    candyColors: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        value: PropTypes.string,
-    })).isRequired,
+    squares: PropTypes.arrayOf(
+        PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    ).isRequired,
 };
 
 function CreateScore(props) {
@@ -92,21 +33,29 @@ CreateScore.propTypes = {
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { score: 0 };
         this.width = 8;
-        this.candyColors = [
-            'url(../images/red-candy.png)',
-            'url(../images/yellow-candy.png)',
-            'url(../images/orange-candy.png)',
-            'url(../images/purple-candy.png)',
-            'url(../images/green-candy.png)',
-            'url(../images/blue-candy.png)',
+        this.candies = [
+            'url("../images/red-candy.png")',
+            'url("../images/yellow-candy.png")',
+            'url("../images/orange-candy.png")',
+            'url("../images/purple-candy.png")',
+            'url("../images/green-candy.png")',
+            'url("../images/blue-candy.png")',
         ];
-        this.squares = document.querySelectorAll('.square');
+        this.state = {
+            score: 0,
+            squares: new Array(64).fill(0).map(() => this.candies[Math.floor(Math.random() * this.candies.length)]),
+        };
         this.checkRowForFour = this.checkRowForFour.bind(this);
         this.checkColumnForFour = this.checkColumnForFour.bind(this);
         this.checkRowForThree = this.checkRowForThree.bind(this);
         this.checkColumnForThree = this.checkColumnForThree.bind(this);
+        this.dragStart = this.dragStart.bind(this);
+        this.dragOver = this.dragOver.bind(this);
+        this.dragEnter = this.dragEnter.bind(this);
+        this.dragLeave = this.dragLeave.bind(this);
+        this.dragDrop = this.dragDrop.bind(this);
+        this.dragEnd = this.dragEnd.bind(this);
     }
 
     componentDidMount() {
@@ -120,28 +69,81 @@ class App extends React.Component {
         }, 200);
     }
 
+    dragStart(e) {
+        this.colorBeingDragged = e.target.style.backgroundImage;
+        this.squareIdBeingDragged = parseInt(e.target.id, 10);
+    }
+
+    dragOver(e) {
+        e.preventDefault();
+    }
+
+    dragEnter(e) {
+        e.preventDefault();
+    }
+
+    dragLeave(e) {
+        e.preventDefault();
+    }
+
+    dragDrop(e) {
+        const { squares } = this.state;
+        this.colorBeingReplaced = e.target.style.backgroundImage;
+        this.squareIdBeingReplaced = parseInt(e.target.id, 10);
+        squares[this.squareIdBeingReplaced] = this.colorBeingDragged;
+        squares[this.squareIdBeingDragged] = this.colorBeingReplaced;
+        this.setState({ squares });
+    }
+
+    dragEnd() {
+        const validMoves = [
+            this.squareIdBeingDragged - 1,
+            this.squareIdBeingDragged - this.width,
+            this.squareIdBeingDragged + 1,
+            this.squareIdBeingDragged + this.width,
+        ];
+        let validMove = validMoves.indexOf(this.squareIdBeingReplaced);
+        if (validMove === 0) {
+            if (this.squareIdBeingReplaced % this.width === 7) validMove = -1;
+        } else if (validMove === 2) {
+            if (this.squareIdBeingReplaced % this.width === 0) validMove = -1;
+        }
+
+        const { squares } = this.state;
+        if (this.squareIdBeingReplaced !== undefined && validMove !== -1) {
+            this.squareIdBeingReplaced = null;
+        } else if (this.squareIdBeingReplaced !== undefined && validMove === -1) {
+            squares[this.squareIdBeingReplaced] = this.colorBeingReplaced;
+            squares[this.squareIdBeingDragged] = this.colorBeingDragged;
+        } else {
+            squares[this.squareIdBeingDragged] = this.colorBeingDragged;
+        }
+        this.setState({ squares });
+    }
+
     moveIntoSquareBelow() {
-        const squares = document.querySelectorAll('.square');
+        const { squares } = this.state;
         for (let i = 0; i < 55; i += 1) {
-            if (squares[i + this.width].style.backgroundImage === '') {
-                squares[i + this.width].style.backgroundImage = squares[i].style.backgroundImage;
-                squares[i].style.backgroundImage = '';
+            if (squares[i + this.width] === '') {
+                squares[i + this.width] = squares[i];
+                squares[i] = '';
             }
             const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
             const isFirstRow = firstRow.includes(i);
-            if (isFirstRow && squares[i].style.backgroundImage === '') {
-                const randomColor = Math.floor(Math.random() * this.candyColors.length);
-                squares[i].style.backgroundImage = this.candyColors[randomColor];
+            if (isFirstRow && squares[i] === '') {
+                const randomColor = Math.floor(Math.random() * this.candies.length);
+                squares[i] = this.candies[randomColor];
             }
         }
+        this.setState({ squares });
     }
 
     checkRowForFour() {
-        const squares = document.querySelectorAll('.square');
+        const { squares } = this.state;
         for (let i = 0; i < 60; i += 1) {
             const rowOfFour = [i, i + 1, i + 2, i + 3];
-            const decidedColor = squares[i].style.backgroundImage;
-            const isBlank = squares[i].style.backgroundImage === '';
+            const decidedColor = squares[i];
+            const isBlank = squares[i] === '';
 
             const notValid = [
                 5,
@@ -166,92 +168,95 @@ class App extends React.Component {
                 54,
                 55,
             ];
-            if (notValid.includes(i)) {
-                continue;
-            }
 
             if (
                 rowOfFour.every(
-                    (index) => squares[index].style.backgroundImage === decidedColor && !isBlank,
-                )
+                    (index) => squares[index] === decidedColor && !isBlank,
+                ) && !notValid.includes(i)
             ) {
-                this.setState((prevState) => ({ score: prevState.score + 4 }));
                 rowOfFour.forEach((index) => {
-                    squares[index].style.backgroundImage = '';
+                    squares[index] = '';
                 });
+                this.setState((prevState) => ({ score: prevState.score + 4 }));
+                this.setState({ squares });
             }
         }
     }
 
     checkColumnForFour() {
-        const squares = document.querySelectorAll('.square');
+        const { squares } = this.state;
         for (let i = 0; i < 39; i += 1) {
             const columnOfFour = [i, i + this.width, i + this.width * 2, i + this.width * 3];
-            const decidedColor = squares[i].style.backgroundImage;
-            const isBlank = squares[i].style.backgroundImage === '';
+            const decidedColor = squares[i];
+            const isBlank = squares[i] === '';
 
             if (
                 columnOfFour.every(
-                    (index) => squares[index].style.backgroundImage === decidedColor && !isBlank,
+                    (index) => squares[index] === decidedColor && !isBlank,
                 )
             ) {
-                this.setState((prevState) => ({ score: prevState.score + 4 }));
                 columnOfFour.forEach((index) => {
-                    squares[index].style.backgroundImage = '';
+                    squares[index] = '';
                 });
+                this.setState((prevState) => ({ score: prevState.score + 4 }));
+                this.setState({ squares });
             }
         }
     }
 
     checkRowForThree() {
-        const squares = document.querySelectorAll('.square');
+        const { squares } = this.state;
         for (let i = 0; i < 61; i += 1) {
             const rowOfThree = [i, i + 1, i + 2];
-            const decidedColor = squares[i].style.backgroundImage;
-            const isBlank = squares[i].style.backgroundImage === '';
+            const decidedColor = squares[i];
+            const isBlank = squares[i] === '';
 
             const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
-            if (notValid.includes(i)) continue;
 
             if (
                 rowOfThree.every(
-                    (index) => squares[index].style.backgroundImage === decidedColor && !isBlank,
-                )
+                    (index) => squares[index] === decidedColor && !isBlank,
+                ) && !notValid.includes(i)
             ) {
-                this.setState((prevState) => ({ score: prevState.score + 3 }));
                 rowOfThree.forEach((index) => {
-                    squares[index].style.backgroundImage = '';
+                    squares[index] = '';
                 });
+                this.setState((prevState) => ({ score: prevState.score + 3 }));
+                this.setState({ squares });
             }
         }
     }
 
     checkColumnForThree() {
-        const squares = document.querySelectorAll('.square');
+        const { squares } = this.state;
         for (let i = 0; i < 47; i += 1) {
             const columnOfThree = [i, i + this.width, i + this.width * 2];
-            const decidedColor = squares[i].style.backgroundImage;
-            const isBlank = squares[i].style.backgroundImage === '';
+            const decidedColor = squares[i];
+            const isBlank = squares[i] === '';
 
             if (
                 columnOfThree.every(
-                    (index) => squares[index].style.backgroundImage === decidedColor && !isBlank,
+                    (index) => squares[index] === decidedColor && !isBlank,
                 )
             ) {
-                this.setState((prevState) => ({ score: prevState.score + 3 }));
                 columnOfThree.forEach((index) => {
-                    squares[index].style.backgroundImage = '';
+                    squares[index] = '';
                 });
+                this.setState((prevState) => ({ score: prevState.score + 3 }));
+                this.setState({ squares });
             }
         }
     }
 
     render() {
-        const { gameScore } = this.state;
+        const { score } = this.state;
+        const { squares } = this.state;
         return (
             <div className="app">
-                <CreateScore score={gameScore} />
-                <CreateBoard width={this.width} candyColors={this.candyColors} />
+                <CreateScore score={score} />
+                <div className="grid" onDragStart={this.dragStart} onDragEnd={this.dragEnd} onDragOver={this.dragOver} onDragEnter={this.dragEnter} onDragLeave={this.dragLeave} onDrop={this.dragDrop}>
+                    <CreateBoard squares={squares} />
+                </div>
             </div>
         );
     }
