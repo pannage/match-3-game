3c20ad73ab6097a663920dde4fca7f21667e0ffd
ekoniuh <dr_zek@mail.ru>
2021-01-26 21:56:45 +0300

    feat: change load field levels

--- src/components/loadLevels.js
+++ src/components/loadLevels.js
@@ -1,272 +1,256 @@
 const candies = [
-    'url(../images/red-candy.png)',
-    'url(../images/yellow-candy.png)',
-    'url(../images/orange-candy.png)',
-    'url(../images/purple-candy.png)',
-    'url(../images/green-candy.png)',
-    'url(../images/blue-candy.png)',
+  'url(../images/red-candy.png)',
+  'url(../images/yellow-candy.png)',
+  'url(../images/orange-candy.png)',
+  'url(../images/purple-candy.png)',
+  'url(../images/green-candy.png)',
+  'url(../images/blue-candy.png)',
 ];
 
 function getNewBoarDataOfGame() {
-    return new Array(8).fill(null).map(() => new Array(8).fill({ type: 1 }).map(() => {
-        const randColor = candies[Math.floor(Math.random() * 6)];
+  return new Array(8).fill(null).map(() =>
+    new Array(8).fill({ type: 1 }).map(() => {
+      const randColor = candies[Math.floor(Math.random() * 6)];
 
-        return {
-            url: randColor,
-            type: candies.indexOf(randColor),
-            toDelete: false,
-            isFrozen: false,
-        };
-    }));
+      return {
+        url: randColor,
+        type: candies.indexOf(randColor),
+        toDelete: false,
+        isFrozen: false,
+      };
+    }),
+  );
 }
 
 function getBoardDataOfLevel1() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            // if (rowId === 3) {
-            //     if (cellId === 3 || cellId === 4) {
-            //         cell.isFrozen = true;
-            //         cell.url = cell.url.replace(/candy.png/, "candy-ice.png");
-            //     }
-            // }
-            // if (rowId === 4) {
-            //     if (cellId === 3 || cellId === 4) {
-            //         cell.isFrozen = true;
-            //         cell.url = cell.url.replace(/candy.png/, "candy-ice.png");
-            //     }
-            // }
-            // if (cellId === 3 || cellId === 4 || rowId === 3 || rowId === 4) {
-            //     cell.isFrozen = true;
-            //     cell.url = cell.url.replace(/candy.png/, "candy-ice.png");
-            // }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (rowId >= 1 && rowId <= 6 && cellId >= 1 && cellId <= 6) {
+        cell.isDesk = true;
+      }
 
-            // if (rowId > 1 && rowId < 6 && cellId > 1 && cellId < 6) {
-            //     cell.isFrozen = true;
-            //     cell.url = cell.url.replace(/candy.png/, "candy-ice.png");
-            // }
-            if (rowId >= 2 && rowId <= 5 && cellId >= 3 && cellId <= 4) {
-                cell.isFrozen = true;
-                cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
-
-                cell.isDesk = true;
-            }
-
-            // cell.isDesk = Math.random() * 100 > 50;
-            // cell.isDesk = true;
-
-            return cell;
-        });
+      return cell;
     });
-    // return boardData.map((row, rowId) => {
-    //     return row.map((cell, cellId) => {
-    //         if (cellId === 0 || cellId === 7 || rowId === 7) {
-    //             cell.isFrozen = true;
-    //             cell.url = cell.url.replace(/candy.png/, "candy-ice.png");
-    //         }
-    //         return cell;
-    //     });
-    // });
+  });
 }
 
 function getBoardDataOfLevel2() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if (rowId === 2 || rowId === 5) {
-                if (cellId === 3 || cellId === 4) {
-                    return {
-                        url: 'url(../images/ground.png)',
-                        type: 'ground',
-                        toDelete: false,
-                        isFrozen: false,
-                    };
-                }
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (cellId === 0 || cellId === 7 || rowId === 7) {
+        cell.isFrozen = true;
+        cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
+      }
 
-            if (rowId === 3 || rowId === 4) {
-                if (cellId === 2 || cellId === 3 || cellId === 4 || cellId === 5) {
-                    return {
-                        url: 'url(../images/ground.png)',
-                        type: 'ground',
-                        toDelete: false,
-                        isFrozen: false,
-                    };
-                }
-            }
+      if (cellId > 1 && cellId < 6 && rowId > 3 && rowId !== 7) {
+        cell.isDesk = true;
+      }
 
-            return cell;
-        });
+      return cell;
     });
+  });
 }
 
 function getBoardDataOfLevel3() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if (cellId === 0 || cellId === 7 || rowId === 7) {
-                cell.isFrozen = true;
-                cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (
+        cellId === 0 ||
+        (cellId === 1 && rowId > 1) ||
+        (cellId === 2 && rowId > 3) ||
+        (cellId === 3 && rowId > 5)
+      ) {
+        return {
+          url: 'url(../images/ground.png)',
+          type: 'ground',
+          toDelete: false,
+          isFrozen: false,
+        };
+      }
 
-            if ((cellId > 1 && cellId < 6) && (rowId > 3 && rowId !== 7)) {
-                cell.isDesk = true;
-            }
+      if (cellId === 6 || cellId === 7) {
+        cell.isDesk = true;
+      }
 
-            return cell;
-        });
+      return cell;
     });
+  });
 }
 
 function getBoardDataOfLevel4() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if (cellId === 0 || (cellId === 1 && rowId > 3) || (cellId === 6 && rowId > 3) || cellId === 7) {
-                return {
-                    url: 'url(../images/ground.png)',
-                    type: 'ground',
-                    toDelete: false,
-                    isFrozen: false,
-                };
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (
+        cellId === 0 ||
+        (cellId === 1 && rowId > 3) ||
+        (cellId === 6 && rowId > 3) ||
+        cellId === 7
+      ) {
+        return {
+          url: 'url(../images/ground.png)',
+          type: 'ground',
+          toDelete: false,
+          isFrozen: false,
+        };
+      }
 
-            if ((cellId > 1 && cellId < 6) && rowId > 3) {
-                cell.isDesk = true;
-            }
+      if (cellId > 1 && cellId < 6 && rowId > 3) {
+        cell.isDesk = true;
+      }
 
-            return cell;
-        });
+      return cell;
     });
+  });
 }
 
 function getBoardDataOfLevel5() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if (cellId === 0 || (cellId === 1 && rowId > 1) || (cellId === 2 && rowId > 3) || (cellId === 3 && rowId > 5)) {
-                return {
-                    url: 'url(../images/ground.png)',
-                    type: 'ground',
-                    toDelete: false,
-                    isFrozen: false,
-                };
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (cellId === 0 || cellId === 7 || rowId === 7) {
+        return {
+          url: 'url(../images/ground.png)',
+          type: 'ground',
+          toDelete: false,
+          isFrozen: false,
+        };
+      }
 
-            if (cellId === 6 || cellId === 7) {
-                cell.isFrozen = true;
-                cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
-            }
+      if ((rowId === 2 || rowId === 3) && (cellId === 3 || cellId === 4)) {
+        cell.isFrozen = true;
+        cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
+      }
 
-            return cell;
-        });
+      if (cellId >= 1 && cellId <= 6 && rowId > 3 && rowId !== 7) {
+        cell.isDesk = true;
+      }
+
+      return cell;
     });
+  });
 }
 
 function getBoardDataOfLevel6() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if (cellId === 0 || cellId === 7 || rowId === 7) {
-                return {
-                    url: 'url(../images/ground.png)',
-                    type: 'ground',
-                    toDelete: false,
-                    isFrozen: false,
-                };
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (rowId > 3 && (cellId === 0 || cellId === 1 || cellId === 6 || cellId === 7)) {
+        cell.isFrozen = true;
+        cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
+      }
 
-            if (rowId === 6) {
-                cell.isFrozen = true;
-                cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
-            }
-
-            return cell;
-        });
+      if (
+        (rowId > 1 && rowId < 6 && (cellId === 2 || cellId === 5)) ||
+        ((rowId === 2 || rowId === 3) && (cellId === 3 || cellId === 4))
+      ) {
+        cell.isDesk = true;
+      }
+      if (
+        ((rowId === 6 || rowId === 7) && cellId > 1 && cellId < 6) ||
+        ((rowId === 4 || rowId === 5) && (cellId === 3 || cellId === 4))
+      ) {
+        return {
+          url: 'url(../images/ground.png)',
+          type: 'ground',
+          toDelete: false,
+          isFrozen: false,
+        };
+      }
+      return cell;
     });
+  });
 }
 
 function getBoardDataOfLevel7() {
-    return getNewBoarDataOfGame().map((row, rowId) => {
-        return row.map((cell, cellId) => {
-            if ((rowId > 4 && (cellId === 2 || cellId === 5)) || (rowId > 3 && (cellId === 3 || cellId === 4)) || rowId === 7) {
-                return {
-                    url: 'url(../images/ground.png)',
-                    type: 'ground',
-                    toDelete: false,
-                    isFrozen: false,
-                };
-            }
+  return getNewBoarDataOfGame().map((row, rowId) => {
+    return row.map((cell, cellId) => {
+      if (rowId > 1) {
+        cell.isDesk = true;
+      }
+      if (
+        (rowId > 4 && (cellId === 2 || cellId === 5)) ||
+        (rowId > 3 && (cellId === 3 || cellId === 4)) ||
+        rowId === 7
+      ) {
+        return {
+          url: 'url(../images/ground.png)',
+          type: 'ground',
+          toDelete: false,
+          isFrozen: false,
+        };
+      }
 
-            if (((rowId === 2 || rowId === 3) && (cellId === 3 || cellId === 4))) {
-                cell.isFrozen = true;
-                cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
-            }
+      if ((rowId === 2 || rowId === 3) && (cellId === 3 || cellId === 4)) {
+        cell.isFrozen = true;
+        cell.url = cell.url.replace(/candy.png/, 'candy-ice.png');
+      }
 
-            return cell;
-        });
+      return cell;
     });
+  });
 }
 
 function checkNumberLevel(target) {
-    // const { target } = event;
-    // if (target.dataset.typeBtn !== "level") return;
-    let result = {};
+  let result = {};
 
-    const { level } = target.dataset;
+  const { level } = target.dataset;
 
-    switch (level) {
+  switch (level) {
     case '1':
-        result = {
-            boardData: getBoardDataOfLevel1(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
+      result = {
+        boardData: getBoardDataOfLevel1(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
 
-        break;
+      break;
     case '2':
-        result = {
-            boardData: getBoardDataOfLevel2(),
-            taskText: '30 turns',
-            moves: 30,
-        };
+      result = {
+        boardData: getBoardDataOfLevel2(),
+        taskText: '30 turns',
+        moves: 30,
+      };
 
-        break;
+      break;
     case '3':
-        result = {
-            boardData: getBoardDataOfLevel3(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
+      result = {
+        boardData: getBoardDataOfLevel3(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
 
-        break;
+      break;
     case '4':
-        result = {
-            boardData: getBoardDataOfLevel4(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
-        break;
+      result = {
+        boardData: getBoardDataOfLevel4(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
+      break;
     case '5':
-        result = {
-            boardData: getBoardDataOfLevel5(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
-        break;
+      result = {
+        boardData: getBoardDataOfLevel5(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
+      break;
     case '6':
-        result = {
-            boardData: getBoardDataOfLevel6(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
-        break;
+      result = {
+        boardData: getBoardDataOfLevel6(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
+      break;
     case '7':
-        result = {
-            boardData: getBoardDataOfLevel7(),
-            taskText: 'delete 10 red',
-            moves: 30,
-        };
-        break;
+      result = {
+        boardData: getBoardDataOfLevel7(),
+        taskText: 'delete 10 red',
+        moves: 30,
+      };
+      break;
     default:
-        break;
-    }
+      break;
+  }
 
-    return result;
+  return result;
 }
 
 export default checkNumberLevel;
