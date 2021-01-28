f385c1e0bae5047decdf7acd4b7c12c856394a92
Tatsiana <pannage@gmail.com>
2021-01-28 20:31:05 +0300

    feat: add win and losw screen

---
+++ src/components/win-screen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/App.css';
+
+export default class WinScreen extends React.Component {
+    render() {
+        return (
+
+            <div className="task-box">
+                <div className="moves-wrap">
+                    <div className="moves">{this.props.moves}</div>
+                    <div className="moves-txt">moves</div>
+                </div>
+                <div className="task">{this.props.message}</div>
+            </div>
+
+        );
+    }
+}
