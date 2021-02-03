79c3e49ab2ac096cedbeb34b2bfea522d85b4a0e
Tatsiana <pannage@gmail.com>
2021-02-03 21:32:34 +0300

    feat: add markup taskbox

--- src/components/task-box.jsx
+++ src/components/task-box.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/App.css';
+
 const candies = [
     'url(../images/red-candy.png)',
     'url(../images/yellow-candy.png)',
@@ -13,13 +14,9 @@ const candies = [
     'url(../images/mine.png)',
     'url(../images/x-bomb.png)',
     'url(../images/xx-bomb.png)',
-  ];
+];
+
 export default class TaskBox extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
     render() {
         return (
             <div className="task-box">
@@ -27,28 +24,35 @@ export default class TaskBox extends React.Component {
                     <div className="moves">{this.props.moves}</div>
                     <div className="moves-txt">moves</div>
                 </div>
-                {this.props.message.map((item) => {
-                    if (item[0] === 'desk') {
+                <div className="task-wrapper">
+                    {this.props.message.map((item) => {
+                        if (item[0] === 'desk') {
+                            return <div className="task" key={item[0]}>
+                                <div className="task-point" style={{ backgroundColor: '#fad69f' }}></div>
+                                <div>{item[1]}</div>
+                            </div>;
+                        }
+
+                        if (item[0] === 'ice') {
+                            return <div className="task" key={item[0]}>
+                                <div className="task-point" style={{ backgroundImage: 'url(../images/blue-candy-ice.png)' }}></div>
+                                <div>{item[1]}</div>
+                            </div>;
+                        }
+
+                        if (item[0] === 'ground') {
+                            return <div className="task" key={item[0]}>
+                                <div className="task-point" style={{ backgroundImage: 'url(../images/ground.png)' }}></div>
+                                <div>{item[1]}</div>
+                            </div>;
+                        }
+
                         return <div className="task" key={item[0]}>
-                        <div style={{ backgroundColor: '#fad69f' }}></div>
-                        <div>{item[1]}</div>
-                    </div>
-                    } else if (item[0] === 'ice') {
-                        return <div className="task" key={item[0]}>
-                        <div style={{ backgroundImage: 'url(../images/blue-candy-ice.png)' }}></div>
-                        <div>{item[1]}</div>
-                    </div>
-                    } else if (item[0] === 'ground') {
-                        return <div className="task" key={item[0]}>
-                        <div style={{ backgroundImage: 'url(../images/ground.png)' }}></div>
-                        <div>{item[1]}</div>
-                    </div>
-                    }
-                    return <div className="task" key={item[0]}>
-                        <div style={{ backgroundImage: candies[item[0]] }}></div>
-                        <div>{item[1]}</div>
-                    </div>
-                })}
+                            <div className="task-point" style={{ backgroundImage: candies[item[0]] }}></div>
+                            <div>{item[1]}</div>
+                        </div>;
+                    })}
+                </div>
             </div>
 
         );
